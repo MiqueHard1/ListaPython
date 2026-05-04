5aed14c001ae7373dce9556bb537ecc04e3a4950
Miqueias Mateus da Silva Xavier <miqueiasmateus256@gmail.com>
2026-05-04 11:29:09 +0000

    v.2

--- seletor.py
+++ seletor.py
@@ -1,1 +1,8 @@
-
+print("🖥️😎Seja Bem Vindo ao Seletor de Caracteres😎🖥️")
+letra = input("Digite a Letra: ")
+if letra == 'A':
+    print("Você Selecionou a opção A ✅")
+elif letra == 'B':
+    print("Você Selecionou a Opção B ✅")
+else:
+    print("Você Selecionou a Opção C ❎")
